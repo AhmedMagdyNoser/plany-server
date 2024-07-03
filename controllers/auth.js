6eb9316696fc0726f3154cacaef42055a8f393d4
Ahmed Magdy <ahmed.magdy.1138@gmail.com>
2024-07-03 11:45:13 +0300

    Add email validation to user registration

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,8 +3,9 @@ const User = require("../models/User");
 module.exports = {
   register: async (req, res) => {
     try {
-      const { firstName, lastName, email, password } = req.body;
-      const user = await User.create({ firstName, lastName, email, password });
+      const existingEmail = await User.findOne({ email: req.body.email });
+      if (existingEmail) return res.status(400).send("Looks like this email already exists.");
+      const user = await User.create(req.body);
       res.status(201).json(user);
     } catch (error) {
       res.status(500).send(error.message);
