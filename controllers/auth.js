53122f0440c1124e5c7a3830d1fce0e96d820acb
Ahmed Magdy <ahmed.magdy.1138@gmail.com>
2024-07-03 14:26:26 +0300

    Refactor the login route to handle JWT generation

--- controllers/auth.js
+++ controllers/auth.js
@@ -11,16 +11,16 @@ module.exports = {
       if (existingEmail) return res.status(409).send("Looks like this email already exists.");
       // Hash the password
       const hashedPassword = await bcrypt.hash(password, 10);
-      // Generate a refresh token and set it as a cookie
+      // Generate a refresh token, set it as a cookie, and save it in the database
       const refreshToken = generateRefreshToken({ email });
       res.cookie("refreshToken", refreshToken, { httpOnly: true, maxAge: process.env.REFRESH_TOKEN_LIFE * 1000 });
-      // Register the user in the database
       const user = await User.create({ firstName, lastName, email, password: hashedPassword, refreshToken });
       // generate an access token and send it as a response
-      const accessToken = generateAccessToken({
-        user: { id: user._id, firstName, lastName, email, createdAt: user.createdAt },
-      });
-      res.status(201).send(accessToken);
+      res.status(201).send(
+        generateAccessToken({
+          user: { id: user._id, firstName, lastName, email, createdAt: user.createdAt },
+        })
+      );
     } catch (error) {
       res.status(500).send(error.message);
     }
@@ -35,13 +35,16 @@ module.exports = {
       // Check if the password is correct
       const validPassword = await bcrypt.compare(password, user.password);
       if (!validPassword) return res.status(401).send("Please verify your email and password and try again.");
-      // Send a response with the user's details excluding the password
-      res.json({
-        firstName: user.firstName,
-        lastName: user.lastName,
-        email: user.email,
-        createdAt: user.createdAt,
-      });
+      // Generate a refresh token, set it as a cookie, and save it in the database
+      const refreshToken = generateRefreshToken({ email });
+      res.cookie("refreshToken", refreshToken, { httpOnly: true, maxAge: process.env.REFRESH_TOKEN_LIFE * 1000 });
+      await User.updateOne({ email }, { refreshToken });
+      // generate an access token and send it as a response
+      res.send(
+        generateAccessToken({
+          user: { id: user._id, firstName: user.firstName, lastName: user.lastName, email, createdAt: user.createdAt },
+        })
+      );
     } catch (error) {
       res.status(500).send(error.message);
     }
