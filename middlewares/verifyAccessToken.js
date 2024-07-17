9f914a442a846d80820a6d7f8ae4283338a8b57f
Ahmed Magdy <ahmed.magdy.1138@gmail.com>
2024-07-17 08:50:56 +0300

    Refactor the verifyAccessToken middleware for better error handling

--- middlewares/verifyAccessToken.js
+++ middlewares/verifyAccessToken.js
@@ -3,12 +3,12 @@ const jwt = require("jsonwebtoken");
 // Middleware to verify the access token to protect the routes and pass the user data to the next middleware.
 
 module.exports = (req, res, next) => {
-  const authHeader = req.headers.authorization || req.headers.Authorization;
+  const authHeader = req.headers.auth;
   const token = authHeader && authHeader.split(" ")[1];
-  if (!token) return res.sendStatus(401);
+  if (!token) return res.status(401).send("No access token provided.");
   // Verify the sent access token
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if (err) return res.sendStatus(403);
+    if (err) return res.status(403).send("Invalid access token.");
     req.user = decoded.user;
     next();
   });
