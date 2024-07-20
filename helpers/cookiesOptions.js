860a9732773aa4a573b77d86dd63bbdd37249691
Ahmed Magdy <ahmed.magdy.1138@gmail.com>
2024-07-20 19:03:27 +0300

    Add sameSite and secure options to cookies
    This commit adds the sameSite and secure options to the cookies in the cookiesOptions.js file. The sameSite option is set to "none" and the secure option is set to true. This is to ensure that the cookies are secure and can only be accessed over HTTPS.

--- helpers/cookiesOptions.js
+++ helpers/cookiesOptions.js
@@ -1,1 +1,1 @@
-module.exports = { httpOnly: true, maxAge: process.env.REFRESH_TOKEN_LIFE * 1000 };
+module.exports = { httpOnly: true, maxAge: process.env.REFRESH_TOKEN_LIFE * 1000, sameSite: "none", secure: true };
